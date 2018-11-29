70286a6682ed66e5875d89c30994cb58ff5d9f00
Molin <molinchenxi@gmail.com>
2018-11-29 14:20:10 +0800

    format annotation

--- src/Helpers/Functions.php
+++ src/Helpers/Functions.php
@@ -24,14 +24,9 @@ if (!function_exists('full_url')) {
     }
 }
 
-
 /**
  * 浏览器友好的变量输出
  * @param mixed $var 变量
- * @param boolean $echo 是否输出 默认为True 如果为false 则返回输出字符串
- * @param string $label 标签 默认为空
- * @param boolean $strict 是否严谨 默认为true
- * @return void|string
  */
 if (!function_exists('dump')) {
     function dump($vars)
@@ -41,7 +36,6 @@ if (!function_exists('dump')) {
             var_dump($var);
             echo '<pre>';
         }
-
     }
 }
 
